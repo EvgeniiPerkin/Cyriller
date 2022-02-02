c8a79ea0317edd805f7d481d84e9e6fc8227ba08
EvgeniiPerkin <evgeniiperkin@bk.ru>
2022-02-02 08:34:01 +0300

    Описание
    Добавил описание класса и реализовал конструкторы

--- Cyriller/src/CyrNameResult.java
+++ Cyriller/src/CyrNameResult.java
@@ -1,10 +1,42 @@
 
 public class CyrNameResult {
 	
+	/**имя*/
 	private String name;
+	/**фамилия*/
 	private String surname;
+	/**отчество*/
 	private String patronumic;
 	
+	/**
+	 * Создает экземпляр класса и заполняет значения surname, name, patronumic
+	 * @param surname - фамилия
+	 * @param name - имя
+	 * @param patronumic - отчество 
+	 * @see CyrNameResult#CyrNameResult(String[])
+	 */
+	public CyrNameResult(String surname, String name, String patronumic) {
+		this.surname = surname;
+		this.name = name;
+		this.patronumic = patronumic;
+	}
+	/**
+	 * Создает экземпляр класса и заполняет значения surname, ame, patronumic из массива
+	 * @param values - массив с инициалами
+	 * @see CyrNameResult#CyrNameResult(String, String, String)
+	 */
+	public CyrNameResult(String[] values) {
+		if (values == null) {
+			throw new NullPointerException();
+		}
+		if (values.length != 2 && values.length != 3){
+			throw new ArrayIndexOutOfBoundsException();
+		}
+	}
+	/**
+	 * Возвращает инициалы в виде строки {@link CyrNameResult#name} +  {@link CyrNameResult#surname} + {@link CyrNameResult#patronumic}
+	 * @return инициалы
+	 */
 	public String getFullName(){
 		String[] values = new String[] {
 				this.surname, this.name, this.patronumic
@@ -31,15 +63,24 @@ public class CyrNameResult {
 	    		&& this.surname.equals(rst.getSurname())
 	    		&& this.patronumic.equals(rst.getPatronumic());
 	}
-	
+	/**
+	 * Возвращает значение из поля {@link CyrNameResult#name}
+	 * @return имя
+	 */
 	public String getName() {
 		return this.name;
 	}
-	
+	/**
+	 * Возвращает значение из поля {@link CyrNameResult#surname}
+	 * @return фамилия
+	 */
 	public String getSurname() {
 		return this.surname;
 	}
-	
+	/**
+	 * Возвращает значение из поля {@link CyrNameResult#patronumic}
+	 * @return отчество
+	 */
 	public String getPatronumic() {
 		return this.patronumic;
 	}
