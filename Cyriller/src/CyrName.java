86d3d1820ac0d052f4d0fd8e78042b2bbfdb728e
EvgeniiPerkin <evgeniiperkin@bk.ru>
2022-02-02 09:11:21 +0300

    Фиксация
    Зафиксировал изменения

--- Cyriller/src/CyrName.java
+++ Cyriller/src/CyrName.java
@@ -1,4 +1,15 @@
 
 public class CyrName {
-
+	public CyrNameResult Decline(String surname, String name, String patronumic, CasesEnum cases, GendersEnum gender, boolean shorten) {
+		return null;
+	}
+	public CyrNameResult Decline(String fullName, CasesEnum cases, GendersEnum gender, boolean shorten) {
+		return null;
+	}
+	public CyrResult Decline(String surname, String name, String patronumic, GendersEnum gender, boolean shorten) {
+		return null;
+	}
+	public CyrResult Decline(String fullName, GendersEnum gender, boolean shorten) {
+		return null;
+	}
 }
---
+++ Cyriller/src/GendersEnum.java
@@ -0,0 +1,11 @@
+
+public enum GendersEnum {
+	/**Неопределенный*/
+	Undefined,
+	/**Мужской*/
+	Masculine,
+	/**Женский*/
+	Feminine,
+	/**Средний*/
+	Neuter
+}
