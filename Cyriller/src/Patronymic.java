f09c967226cadbc71a0561da709ee8f3232db77f
EvgeniiPerkin <evgeniiperkin@bk.ru>
2022-02-07 20:51:08 +0300

    фиксация
    фиксация изменений/добавлений

---
+++ Cyriller/src/Patronymic.java
@@ -0,0 +1,25 @@
+
+public class Patronymic {
+	private String prefix;
+	private String patronymic;
+	private String suffix;
+	
+	public String getPrefix() {
+		return prefix;
+	}
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+	public String getPatronymic() {
+		return patronymic;
+	}
+	public void setPatronymic(String patronymic) {
+		this.patronymic = patronymic;
+	}
+	public String getSuffix() {
+		return suffix;
+	}
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+}
