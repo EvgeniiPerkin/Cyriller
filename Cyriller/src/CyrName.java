b127d1e1796df9743c58fd19a2c0bb03c4055a2e
EvgeniiPerkin <evgeniiperkin@bk.ru>
2022-02-09 08:48:56 +0300

    фиксация
    фиксация добавления/изменения

--- Cyriller/src/CyrName.java
+++ Cyriller/src/CyrName.java
@@ -1,3 +1,4 @@
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CyrName {
@@ -5,8 +6,8 @@ public class CyrName {
 	private Pattern patronymicPrefixRegex;
 
 	public CyrName() {
-		patronymicSuffixRegex = Pattern.compile("([- ](((оглы)|(улы)|(уулу))|([кг]ызы)))$");
-		patronymicPrefixRegex = Pattern.compile("^(ибн[- ])");
+		patronymicSuffixRegex = Pattern.compile("((?i)[- ](((?i)(оглы)|(улы)|(уулу))|([(?i)кг](?i)ызы)))$", Pattern.UNICODE_CASE);
+		patronymicPrefixRegex = Pattern.compile("^((?i)ибн[- ])", Pattern.UNICODE_CASE);
 	}
 	
 	/**
@@ -64,12 +65,128 @@ public class CyrName {
         name = this.ProperCase(inputName);
         
         Patronymic p =  SplitPatronymic(inputPatronymic);
+        patronymic = p.getPatronymic();
+        patronymicAfter = p.getSuffix();
+        patronymicBefore = p.getPrefix();
+        
+        declinePatronymic = patronymicAfter == null || patronymicAfter.isEmpty();
+        patronymic = this.ProperCase(patronymic);
+        
+        if (caseNumber < 1 || caseNumber > 6){
+            caseNumber = 1;
+        }
+
+        if (gender < 0 || gender > 2){
+            gender = 0;
+        }
+        
+        if (gender == 0){
+            gender = patronymic.endsWith("на") ? 2 : 1;
+        }
+
+        isFeminine = (gender == 2);
+
+        surnameOld = surname;
+        surnameNew = "";
+        index = surnameOld.indexOf("-");
+        
+        while (index > 0){
+            temp = this.ProperCase(surnameOld.substring(0, index));
+            surnameNew = surnameNew + this.DeclineSurname(temp, caseNumber, isFeminine) + "-";
+            surnameOld = surnameOld.substring(index + 1);
+            index = surnameOld.indexOf("-");
+        }
+
+        temp = this.ProperCase(surnameOld);
+        surnameNew = surnameNew + this.DeclineSurname(temp, caseNumber, isFeminine);
+        surname = surnameNew;
         
 		return null;
 	}
 	public String[] Decline(String fullName, int cases, int gender, boolean shorten) {
 		return null;
 	}
+	/**
+	 * Склоняет фамилию в указанный падеж.
+	 * @param surname Фамилия, в именительном падеже.
+	 * @param cases Падеж, в который нужно просклонять фамилию, где 1 – именительный, 2 – родительный, 3 – дательный, 4 – винительный, 5 – творительный, 6 – предложный.
+	 * @param isFeminine True, для женских фамилий.
+	 * @return Возвращает результат склонения.
+	 */
+	public String DeclineSurname(String surname, int cases, boolean isFeminine) {
+		String result = surname;
+		
+		switch (cases){
+            case 2:
+                result = this.DeclineSurnameGenitive(surname, isFeminine);
+                break;
+            case 3:
+                result = this.DeclineSurnameDative(surname, isFeminine);
+                break;
+            case 4:
+                result = this.DeclineSurnameAccusative(surname, isFeminine);
+                break;
+            case 5:
+                result = this.DeclineSurnameInstrumental(surname, isFeminine);
+                break;
+            case 6:
+                result = this.DeclineSurnamePrepositional(surname, isFeminine);
+                break;
+        }
+
+        return result;
+	}
+	/**
+	 * Предложный, О ком? О чем? (думаю)
+	 * @param surname Фамилия, для склонения.
+	 * @param isFeminine True, для женских фамилий.
+	 * @return Возвращает результат склонения.
+	 */
+	public String DeclineSurnamePrepositional(String surname, boolean isFeminine) {
+		return null;
+	}
+	/**
+	 * Творительный, Кем? Чем? (горжусь)
+	 * @param surname Фамилия, для склонения.
+	 * @param isFeminine True, для женских фамилий.
+	 * @return Возвращает результат склонения.
+	 */
+	public String DeclineSurnameInstrumental(String surname, boolean isFeminine) {
+		return null;
+	}
+	/**
+	 * Винительный, Кого? Что? (вижу)
+	 * @param surname Фамилия, для склонения.
+	 * @param isFeminine True, для женских фамилий.
+	 * @return Возвращает результат склонения.
+	 */
+	public String DeclineSurnameAccusative(String surname, boolean isFeminine) {
+		return null;
+	}
+	/**
+	 * Дательный, Кому? Чему? (дам)
+	 * @param surname Фамилия, для склонения.
+	 * @param isFeminine True, для женских фамилий.
+	 * @return Возвращает результат склонения.
+	 */
+	public String DeclineSurnameDative(String surname, boolean isFeminine) {
+		return null;
+	}
+	/**
+	 * Родительный, Кого? Чего? (нет)
+	 * @param surname Фамилия, для склонения.
+	 * @param isFeminine True, для женских фамилий.
+	 * @return Возвращает результат склонения.
+	 */
+	public String DeclineSurnameGenitive(String surname, boolean isFeminine) {
+		return null;
+	}
+
+	/**
+	 * Используется для разбивки составных отчеств, к примеру тюркские варианты Салим-оглы или Салим-кызы.
+	 * @param fullPatronymic Полное отчество. Примеры: "Салим-оглы", "Салим Оглы", "Ибн Салим".
+	 * @return возвращает отчество
+	 */
 	public Patronymic SplitPatronymic(String fullPatronymic) {
 		Patronymic result = new Patronymic();
 		if (fullPatronymic == null || fullPatronymic.isEmpty()) {
@@ -80,8 +197,36 @@ public class CyrName {
 		}
 
 		result.setPatronymic(fullPatronymic);
-		
-		return null;
+
+        Matcher prefixMatch = this.patronymicPrefixRegex.matcher(fullPatronymic);
+        Matcher suffixMatch = this.patronymicSuffixRegex.matcher(fullPatronymic);
+
+        result.setPatronymic(fullPatronymic);
+        
+        boolean isPrefix = false;
+        
+        while (prefixMatch.find()){
+        	result.setPrefix(prefixMatch.group());
+        	result.setPatronymic(prefixMatch.replaceAll(""));
+            isPrefix = true;
+	    }
+	        
+        if (!isPrefix){
+        	result.setPrefix(null);
+        }
+        
+        boolean isSuffiix = false;
+        
+        while (suffixMatch.find()){
+        	result.setSuffix(suffixMatch.group());
+        	result.setPatronymic(suffixMatch.replaceAll(""));
+            isSuffiix = true;
+	    }
+        if (!isSuffiix){
+        	result.setSuffix(null);
+        }
+        
+		return result;
 	}
 	public String ProperCase(String value) {
 		if (value != null) {
--- Cyriller/src/Patronymic.java
+++ Cyriller/src/Patronymic.java
@@ -1,24 +1,54 @@
-
+/**
+ * оболочка для отчества
+ * @author Perkin
+ *
+ */
 public class Patronymic {
+	/**часть составного отчества, которая не должна склоняться*/
 	private String prefix;
+	/**часть отчества, которая может склоняться.*/
 	private String patronymic;
+	/**часть составного отчества, которая не должна склоняться*/
 	private String suffix;
-	
+	/**
+	 * Возвращает префиксную часть составного отчества, которая не должна склоняться.
+	 * @return
+	 */
 	public String getPrefix() {
 		return prefix;
 	}
+	/**
+	 * Устанавливает префиксную часть составного отчества, которая не должна склоняться.
+	 * @param prefix
+	 */
 	public void setPrefix(String prefix) {
 		this.prefix = prefix;
 	}
+	/**
+	 * Возвращает основную часть отчества, которая может склоняться.
+	 * @return
+	 */
 	public String getPatronymic() {
 		return patronymic;
 	}
+	/**
+	 * Устанавливает основную часть отчества, которая может склоняться.
+	 * @param patronymic
+	 */
 	public void setPatronymic(String patronymic) {
 		this.patronymic = patronymic;
 	}
+	/**
+	 * Возвращает суффиксную часть составного отчества, которая не должна склоняться.
+	 * @return
+	 */
 	public String getSuffix() {
 		return suffix;
 	}
+	/**
+	 * Устанавливает суффиксную часть составного отчества, которая не должна склоняться.
+	 * @param suffix
+	 */
 	public void setSuffix(String suffix) {
 		this.suffix = suffix;
 	}
