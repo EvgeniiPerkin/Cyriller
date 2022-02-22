0b65ee390d1d9703ccd120a5b588b9909d401a21
DevelopmentCapitalBank <epsnak@gmail.com>
2022-02-22 16:53:31 +0300

    df23
    sdddd33

---
+++ Cyriller/src/CyrNameTest.java
@@ -0,0 +1,162 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CyrNameTest {
+
+	@Test
+	void testCyrName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineStringStringStringCasesEnumGendersEnumBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineStringCasesEnumGendersEnumBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineStringStringStringGendersEnumBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineStringGendersEnumBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineStringStringStringIntIntBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineStringIntIntBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineNameGenitive() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineNameDative() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineNameAccusative() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineNameInstrumental() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineNamePrepositional() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclinePatronymicGenitive() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclinePatronymicDative() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclinePatronymicAccusative() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclinePatronymicInstrumental() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclinePatronymicPrepositional() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineSurname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineSurnamePrepositional() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineSurnameInstrumental() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineSurnameAccusative() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineSurnameDative() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeclineSurnameGenitive() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSplitPatronymic() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testProperCase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEndStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEndStringIntString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSubstringRight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testShorten() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsNullOrWhitespace() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsShorten() {
+		fail("Not yet implemented");
+	}
+
+}
