fc1b3f124de3db12429b701e5a9ee8077cb33ee7
DevelopmentCapitalBank <epsnak@gmail.com>
2022-03-01 13:13:11 +0300

    99
    77

--- Cyriller/src/main/Main.java
+++ Cyriller/src/main/Main.java
@@ -25,7 +25,7 @@ public class Main {
         for(String d : df) {
 			System.out.println(d);
 		}
-        /*986lksadfasdf47 ----lkjlkjlkjlkj helloddd*/
+        /*986lksadfasdf47 ----lkjlkjlkjlkj ddhelloddd*/
 	}
 
 }
